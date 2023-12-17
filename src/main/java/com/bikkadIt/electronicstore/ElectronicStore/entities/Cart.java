d1c73d0fbf7d9ef495a9c90ab07673cc0f77ce06
panchafula <=>
2023-12-17 17:31:50 +0000

    create Entity for Cart

---
+++ src/main/java/com/bikkadIt/electronicstore/ElectronicStore/entities/Cart.java
@@ -0,0 +1,26 @@
+package com.bikkadIt.electronicstore.ElectronicStore.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Carts")
+@Builder
+public class Cart {
+    @Id
+    private String cartId;
+    private Date createAt;
+    @OneToOne
+    private User user;
+
+    // mapping cart Item
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<CartItem> items=new ArrayList<>();
+}
