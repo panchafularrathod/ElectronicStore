8b79bb2a006ec46eda5f8909cde6816d5aef9568
panchafula <=>
2023-12-28 03:48:15 +0000

    crete class SecurityConfig and crate bean for passwordEncoder and DaoAuthenticationProvider

---
+++ src/main/java/com/bikkadIt/electronicstore/ElectronicStore/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.bikkadIt.electronicstore.ElectronicStore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(this.userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+}
