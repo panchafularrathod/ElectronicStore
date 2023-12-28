083efb9b895337f28a8f8e957523a0741bdb3954
panchafula <=>
2023-12-28 03:13:24 +0000

    modification in User class implement UserDetails interface and its method

--- src/main/java/com/bikkadIt/electronicstore/ElectronicStore/entities/User.java
+++ src/main/java/com/bikkadIt/electronicstore/ElectronicStore/entities/User.java
@@ -1,9 +1,12 @@
 package com.bikkadIt.electronicstore.ElectronicStore.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -13,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 
-public class User {
+public class User implements UserDetails {
 
 
         @Id
@@ -42,6 +45,41 @@ public class User {
 
         @OneToMany(mappedBy = "user",fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
         private List<Order> orders = new ArrayList<>();
-    }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+        }
+
+        @Override
+        public String getUsername() {
+                return this.email;
+        }
+
+        @Override
+        public String getPassword(){
+
+                return this.password;
+        }
+        @Override
+        public boolean isAccountNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+                return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+                return true;
+        }
+}
 
 
